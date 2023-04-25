08a1d3a0420cab7521bdce9bc7eae8b24f543fd0
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-04-25 14:56:59 +0300

    metin belgesine kayıt

---
+++ Dosya_islemleri/Metin_belgesi_kayıt/Metin_belgesi_kayıt/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Metin_belgesi_kayıt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            saveFileDialog1.Filter = "Metin Dosyaları|*.txt";
+            saveFileDialog1.Title = "Metin belgesi kayıt";        
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw= new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            MessageBox.Show("Belge oluşturuldu.");
+            
+            
+        }
+    }
+}
